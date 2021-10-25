bb3a1d0b294f7fed9f34a5bf850b4a8b7fdbaa58
MatveenkovNikita <matveenkoff@gmail.com>
2021-10-25 23:14:43 +0300

    Initial Commit

---
+++ NewsTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTestApp
+//
+//  Created by Никита  on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
